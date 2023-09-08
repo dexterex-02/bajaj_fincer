93756b245c3398940690945852a98f0ecb8f0f42
HIMANSHU AGRAWAL <himanshuagrawal72539@gmail.com>
2023-09-08 16:08:41 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = require('./route.js');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.use(router);
+
+app.get('/', async (req, res) =>
+{
+    res.status(201).send('Hello world!');
+});
+
+app.listen(port, () =>
+{
+    console.log(`Server is live at port no. ${port}`);
+});
